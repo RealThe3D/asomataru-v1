6854bcb600ab736bd01731c0758cc81b3fcd0f36
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-07-24 02:08:26 -0400

    Updated statement for Mochi

--- commands/credits.js
+++ commands/credits.js
@@ -6,7 +6,7 @@ module.exports = {
         const credits = [
             "Davska#6229 - Pro smash player, true friend, and loves the Sinnoh region!",
             "Kandrina#1426 - Lovely woman from Austria, likes eating ribs!",
-            "Mochi#7342 - Coolio person, like to eat off of DoorDash and Postmates.",
+            "Mochi#7342 - Coolio person, loves to eat mochi.",
             "オルティナ#6718 aka Creation, worthy player and waifu!",
             "Incineroar#7934 - Fatal memer and pro smash player",
             "SteMi#6098 - Busy with schoolwork but can manage to tell me oyasumi \o/",
