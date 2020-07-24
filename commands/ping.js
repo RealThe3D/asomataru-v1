8807419d311dcc79b9a9f642343a36d3d381ffe5
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-07-24 11:52:32 -0400

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,20 @@ module.exports = {
     aliases: 'pong',
     description: 'Ping!',
     execute: async (message, args) => {
-        const m = await message.channel.send("Ping?");
-        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. 🏓`);
+        const Discord = require("discord.js");
+        const package = require('../package.json');
+        const version = (package.version);
+        const ping = new Discord.RichEmbed()
+            .setTitle("Ping")
+            .setDescription(`Ping?`)
+            .setColor(0xDFF8EB)
+            .setFooter(`Version ${version}`)
+        const m = await message.channel.send(ping);
+        const pong = new Discord.MessageEmbed()
+            .setTitle("Ping")
+            .setDescription(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. 🏓`)
+            .setColor(0xDFF8EB)
+            .setFooter(`Version ${version}`)
+        m.edit(pong);
     },
 };
