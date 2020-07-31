45bfdec4823fd4d9b66c8ce338a769da623df1d9
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-07-31 13:16:49 -0400

    Edited
    Accidently put http instead of https for link for cat.js, dog.js

--- commands/birthday.js
+++ commands/birthday.js
@@ -1,18 +1,6 @@
 module.exports = {
     name: 'birthday',
     execute: async (message, args) => {
-        const ID = "564755874736898048"; 
-        const Owner = "327594208758202379";
-        const Discord = require('discord.js');
-
-        const bDayEmbed = new Discord.MessageEmbed()
-        .setImage ("https://images.indianexpress.com/2017/01/happy-birthday-wish_1500_thinkstockphotos-531424610.jpg")
-        .setTitle("**Happy Birthday, Koro!**")
-        
-        if (message.author.id === ID || Owner) {
-            message.channel.send(bDayEmbed);
-        } else {
-            return message.channel.send("DM happy birthday to SpaceLives47#0463!");
-            }
-        },
-    };
+       message.channel.send("Birthday is over with, proceed with everything else to offer")
+    },
+};
