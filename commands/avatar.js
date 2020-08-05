bf68c1100e8268c972cec4e6998b60d8dfd5811a
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-08-04 20:21:29 -0400

    Rollback

--- commands/avatar.js
+++ commands/avatar.js
@@ -7,7 +7,7 @@ module.exports = {
         const avatarEmbed = new Discord.MessageEmbed()
             .setColor(0x333333)
             .setAuthor(`${user.username}'s Profile Picture`)
-            .setImage(user.displayAvatarURL({format: 'gif'}));
+            .setImage(user.displayAvatarURL({format: 'jpg'}));
         message.channel.send(avatarEmbed);
     },
 };
