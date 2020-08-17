1c39c774f72d0c7857513afbde1c1f654aad1c88
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-08-17 15:38:12 -0400

    Experiment

--- bot.js
+++ bot.js
@@ -1,70 +1,49 @@
 const Discord = require("discord.js");
-const package = require('./package.json');
-const version = (package.version)
+const client = new Discord.Client({ disableMentions: "everyone" }); //remove the parameters being passed into 
+const prefix = process.env.PREFIX;
+const token = proces..env.BOT_TOKEN;
 const fs = require("fs");
-const TOKEN = process.env.BOT_TOKEN;
-const prefix = process.env.BOT_PREFIX;
 
-const bot = new Discord.Client();
-bot.commands = new Discord.Collection();
+/**
+ * If you need to use other databases or apis that require credentials,
+ * you should place them here and add them as a property in the bot object
+ */
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+/**
+ * Tips for database/storage
+ * If you want a local SQL database, I highly recommend sqlite3
+ *      https://www.sqlite.org/index.html https://www.npmjs.com/package/better-sqlite3
+ *      npm i sqlite3 && better-sqlite3
+ * 
+ * If you want to use a cloud no-sql database (syncable across multiple instances/bots), I recommend google firestore or mongoDB
+ *      https://firebase.google.com/docs/firestore https://www.mongodb.com/
+ */
 
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    bot.commands.set(command.name, command);
-}
+let bot = {client, config, prefix};
+/**
+ * These add commands, aliases, events, and functions to the client object
+ * Discord Collections work like a Map/Dictionary Object
+ * To get a specific function from the collection, use 
+ * client.xxxxxxx.get("name");
+ */
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.events = new Discord.Collection();
+client.functions = new Discord.Collection();
+client.categories = fs.readdirSync("./commands/");
 
+/**
+ * These functions are used to grab the modules in each corresponding folder
+ * whenever the loadxxx() functions are called, the cache will be cleared of the previous data 
+ * and the new data will be loaded into the collections so you can update code without restarting the
+ * entire bot
+ */
+client.loadCmds = (client, reload) => require(`./handlers/command`)(client, reload);
+client.loadFunctions = (client, reload) => require(`./handlers/function`)(client, reload);
+client.loadEvents = (client, reload) => require("./handlers/event.js")(client, reload, bot);
+client.loadCmds(client, false);
+client.loadFunctions(client, false);
+client.loadEvents(client, false);
 
-bot.once('ready', () => {
-    console.log(`Bot is ready and working in ${bot.guilds.size} servers with ${bot.users.size} users! The bot is on version ${version}`);
-    bot.user.setActivity(`Asomataru v1.1.7.1!`);
-    bot.user.setStatus('online', 'Made by The3D#9801')
-    console.log('Ready!');
-});
-
-bot.on("guildCreate", guild => {
-    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${bot.memberCount} members!`);
-    console.log(`I am now working in ${bot.guilds.size} servers with ${bot.users.size} users!`)
-});
-
-bot.on("guildDelete", guild => {
-    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-    console.log(`I am now working in ${bot.guilds.size} servers with ${bot.users.size} users!`)
-});
-
-bot.on('message', async (message) => {
-	if (message.channel.type === "dm") return;
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
-
-	const command =
-		bot.commands.get(commandName) ||
-		bot.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
-
-	if (!command) return;
-
-	if (command.guildOnly && message.channel.type !== 'text') {
-		return message.reply("I can't execute that command inside DMs!");
-	}
-
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`;
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
-
-		return message.channel.send(reply);
-	}
-
-	try {
-		command.execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('There was an error trying to execute that command!');
-	}
-});
-bot.login(TOKEN);
+module.exports = bot;
+client.login(token);
