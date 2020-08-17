07aa5593084390ffa3881c69de146887c0c68ea4
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-08-17 16:47:29 -0400

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -7,7 +7,7 @@ module.exports = {
         const avatarEmbed = new Discord.MessageEmbed()
             .setColor(0x333333)
             .setAuthor(`${user.username}'s Profile Picture`)
-            .setImage(user.displayAvatarURL({format: 'jpg'}));
+            .setImage(user.avatarURL({size: 2048, dynamic: true})) 
         message.channel.send(avatarEmbed);
     },
 };
