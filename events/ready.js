354ab3d247831c602343b39ed6781a50ac2ac1c5
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-08-17 15:39:46 -0400

    Experiemental

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,16 @@
-// soon.
+
+module.exports = {
+	name: "ready",
+	func: runAll,
+};
+function runAll(bot) {
+	login(bot);
+}
+function login(bot){
+    const {client} = bot;
+    console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    console.log("")
+    console.log(`   Logged in as ${client.user.tag}!   `);
+    console.log("")
+    console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+}
